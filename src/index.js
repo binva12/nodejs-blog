3503455792c3bf5a95b5e47289d68e24d85782ab
binva12 <quocdat3b@yahoo.com.vn>
2022-05-16 13:23:14 +0700

    test lint-stagedm husky

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ app.use(
 /* app.use(morgan('combined')); */
 
 // Template engine
-app.engine('hbs', hbs.engine({ extname: '.hbs' }))
+pp.engine('hbs', hbs.engine({ extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.set('views', path.join(__dirname, 'resources/views'))
 
